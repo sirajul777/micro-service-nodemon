e937446a216c2ada97f16000b487cd65a71577d3
jull <50972782+sirajul777@users.noreply.github.com>
2026-08-16 22:55:54 +0800

    feat(erp): include voucher batch grpc module

--- erp-node-service/src/internal-grpc.module.ts
+++ erp-node-service/src/internal-grpc.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { InternalGrpcController } from './internal-grpc.controller';
 import { ErpSessionStore } from './internal-grpc.store';
 import { MikrotikGrpcClient } from './clients/mikrotik-grpc.client';
+import { VoucherBatchInternalController } from './internal-grpc/voucher-batch.controller';
+import { VoucherBatchInternalStore } from './internal-grpc/voucher-batch.store';
+import { VoucherBatchService } from './voucher-batch/voucher-batch.service';
 
 @Module({
-  controllers: [InternalGrpcController],
-  providers: [ErpSessionStore, MikrotikGrpcClient],
-  exports: [ErpSessionStore],
+  controllers: [InternalGrpcController, VoucherBatchInternalController],
+  providers: [ErpSessionStore, MikrotikGrpcClient, VoucherBatchInternalStore, VoucherBatchService],
+  exports: [ErpSessionStore, VoucherBatchInternalStore],
 })
 export class InternalGrpcModule {}
