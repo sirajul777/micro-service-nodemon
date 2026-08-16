23cbe59c6e03aafb37bc01b50824d1bc6ed32303
jull <50972782+sirajul777@users.noreply.github.com>
2026-08-16 23:10:26 +0800

    feat(erp): register payment voucher type grpc handler

--- erp-node-service/src/internal-grpc.module.ts
+++ erp-node-service/src/internal-grpc.module.ts
@@ -6,18 +6,22 @@ import { VoucherBatchInternalController } from './internal-grpc/voucher-batch.co
 import { VoucherBatchInternalStore } from './internal-grpc/voucher-batch.store';
 import { VoucherBatchService } from './voucher-batch/voucher-batch.service';
 import { VoucherGenerateInternalController } from './internal-grpc/voucher-generate.controller';
+import { PaymentVoucherTypeInternalController } from './internal-grpc/payment-voucher-type.controller';
+import { VoucherTypeService } from './voucher-type/voucher-type.service';
 
 @Module({
   controllers: [
     InternalGrpcController,
     VoucherBatchInternalController,
     VoucherGenerateInternalController,
+    PaymentVoucherTypeInternalController,
   ],
   providers: [
     ErpSessionStore,
     MikrotikGrpcClient,
     VoucherBatchInternalStore,
     VoucherBatchService,
+    VoucherTypeService,
   ],
   exports: [ErpSessionStore, VoucherBatchInternalStore],
 })
