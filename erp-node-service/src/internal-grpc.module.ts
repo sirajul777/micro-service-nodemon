293b9d2d06177918f270c7dec9b5abeed708fcd4
jull <50972782+sirajul777@users.noreply.github.com>
2026-08-19 01:35:52 +0800

    fix(erp): wire voucher batch dependencies into internal grpc module

--- erp-node-service/src/internal-grpc.module.ts
+++ erp-node-service/src/internal-grpc.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { InternalGrpcController } from './internal-grpc.controller';
 import { ErpSessionStore } from './internal-grpc.store';
 import { MikrotikGrpcClient } from './clients/mikrotik-grpc.client';
 import { VoucherBatchInternalController } from './internal-grpc/voucher-batch.controller';
 import { VoucherBatchInternalStore } from './internal-grpc/voucher-batch.store';
 import { VoucherBatchService } from './voucher-batch/voucher-batch.service';
+import { VoucherBatchCreationService } from './voucher-batch/voucher-batch-creation.service';
 import { VoucherGenerateInternalController } from './internal-grpc/voucher-generate.controller';
 import { PaymentVoucherTypeInternalController } from './internal-grpc/payment-voucher-type.controller';
 import { VoucherTypeInternalController } from './internal-grpc/voucher-type.controller';
 import { VoucherTypeService } from './voucher-type/voucher-type.service';
+import { ProfileMetaService } from './profile-meta/profile-meta.service';
+import { VoucherBatchEntity } from './entities/voucher-batch.entity';
+import { ProfileMetaEntity } from './entities/profile-meta.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([VoucherBatchEntity, ProfileMetaEntity]),
+  ],
   controllers: [
     InternalGrpcController,
     VoucherBatchInternalController,
@@ -23,6 +31,8 @@ import { VoucherTypeService } from './voucher-type/voucher-type.service';
     MikrotikGrpcClient,
     VoucherBatchInternalStore,
     VoucherBatchService,
+    VoucherBatchCreationService,
+    ProfileMetaService,
     VoucherTypeService,
   ],
   exports: [ErpSessionStore, VoucherBatchInternalStore],
