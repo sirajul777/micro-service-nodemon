0cdd291b6115aabd0c4b0ac46edd6297cb3828d1
jull <50972782+sirajul777@users.noreply.github.com>
2026-08-19 02:17:02 +0800

    fix(payment): expose payment services to grpc controller

--- payment-service/src/internal-grpc.module.ts
+++ payment-service/src/internal-grpc.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { InternalGrpcController } from './internal-grpc.controller';
+import { PaymentModule } from './payment.module';
 
 @Module({
+  imports: [PaymentModule],
   controllers: [InternalGrpcController],
 })
 export class InternalGrpcModule {}
